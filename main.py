efb747b1ebb521a22c68bba40596a53d37ae8928
fmorand29 <fmorand29@gmail.com>
2020-03-29 15:41:56 +0200

    environnement
    Le plateau de jeu

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import pygame
+from direct.showbase.ShowBase import ShowBase
 
-
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.keyMap = {
+            "up" : False,
+            "down" : False,
+            "left" : False,
+            "right" : False,
+            "shoot" : False
+                        }
+        base.setFrameRateMeter(True)  # mesure le FPS
+        # Création du plateau de jeu
+        self.environment = loader.loadModel("./Environment/environment")
+        self.environment.reparentTo(render)
+        # Positionnement de la caméra
+        
+        
+game = Game()
+game.run()
